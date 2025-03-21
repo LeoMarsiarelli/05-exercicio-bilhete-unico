dde1e30a837c1813f7bb39ad0007755a5666ee57
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-20 21:02:41 -0300

    Bilhete Unico

---
+++ 05-exercicio-bilhete-unico-main/src/Usuario.java
@@ -0,0 +1,13 @@
+public class Usuario {
+    String nome;
+    String perfil;
+    long cpf;
+
+
+    public Usuario(String nome, String perfil, long cpf) {
+        this.nome = nome;
+        this.perfil = perfil;
+        this.cpf = cpf;
+    }
+
+}
